aeda4b9f51e81cb97e38577a741ed3e5be9c7c4c
Сергей Лешуков <lsl775319@gmail.com>
2023-03-09 20:21:36 +0300

    модификиция

--- src/test/java/ru/netology/delivery/test/DeliveryTest.java
+++ src/test/java/ru/netology/delivery/test/DeliveryTest.java
@@ -21,17 +21,8 @@ class DeliveryTest {
     @Test
     @DisplayName("Should successful plan and replan meeting")
     void shouldSuccessfulPlanAndReplanMeeting() {
-       // var validUser = DataGenerator.Registration.generateUser("RU");
-       // var daysToAddForFirstMeeting = 4;
-       // var firstMeetingDate = DataGenerator.generateDate(daysToAddForFirstMeeting);
-       // var daysToAddForSecondMeeting = 7;
-       // var secondMeetingDate = DataGenerator.generateDate(daysToAddForSecondMeeting);
-        // TODO: добавить логику теста в рамках которого будет выполнено планирование и перепланирование встречи.
-        // Для заполнения полей формы можно использовать пользователя validUser и строки с датами в переменных
-        // firstMeetingDate и secondMeetingDate. Можно также вызывать методы generateCity(locale),
-        // generateName(locale), generatePhone(locale) для генерации и получения в тесте соответственно города,
-        // имени и номера телефона без создания пользователя в методе generateUser(String locale) в датагенераторе
-        DataGenerator.UserInfo validUser = DataGenerator.Registration.generateUser("ru");
+
+        DataGenerator.UserInfo validUser = DataGenerator.Registration.generateUser("RU");
         int daysToAddForFirstMeeting = 4;
         String firstMeetingDate = DataGenerator.generateDate(daysToAddForFirstMeeting);
         int daysToAddForSecondMeeting = 7;
@@ -50,10 +41,13 @@ class DeliveryTest {
         $x("//span[@data-test-id= 'date']//input").sendKeys(Keys.chord(Keys.SHIFT,Keys.HOME),Keys.DELETE);
         $x("//span[@data-test-id= 'date']//input").setValue(secondMeetingDate);
         $x("//span[contains(text(),  'Запланировать')]").click();
+        //$("[data-test-id='success-notification'] .notification__content").shouldBe(visible, Duration.ofSeconds(15));
         $("[data-test-id='replan-notification'] .notification__content")
                 .shouldHave(exactText("У вас уже запланирована встреча на другую дату. Перепланировать?"))
                 .shouldBe(visible);
-        $x("//div//span[contains(text(), 'Перепланировать')]").click();
+        // $x("//div//span[contains(text(), 'Перепланировать')]").click();
+        // $x("//div[@data-test-id='replan-notification']//button").click();
+        $("[data-test-id='replan-notification'] button").click();
         $("[data-test-id='success-notification'] .notification__content")
                 .shouldHave(exactText("Встреча успешно запланирована на " + secondMeetingDate))
                 .shouldBe(visible);
